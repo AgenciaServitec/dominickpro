b472c24ac5ca2d0284d6536e3e7acafc15576b5c
Angel Gala <galafloresangelemilio@gmail.com>
2026-05-18 12:31:34 -0500

    added 404 page and added site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://proyecto-inmobiliario-dpi.com',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
